c23051adf1673d87e514bbf1b3b7d630754e8c56
Manoj Kumar Vasudevan <117102195+manopavit@users.noreply.github.com>
2024-11-02 11:47:23 +0400

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,7 +6,7 @@ def testApp(){
 }
 def deployApp(){
   echo 'deploying the application..'
-  echo "deploying version ${params.VERSION}'
+  echo "deploying version ${params.VERSION}"
 }
 
 return this
